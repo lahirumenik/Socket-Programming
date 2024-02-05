39b96b987ef6d2b1d6a362fad227ae71d36d4a6e
lahirumenik <115687865+lahirumenik@users.noreply.github.com>
2024-02-05 14:21:32 +0530

    add server details

--- socketclient/main.c
+++ socketclient/main.c
@@ -3,3 +3,16 @@
 //
 #include <stdio.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(){
+    //AF_INET -> IPV4, SOCK_STREAM -> TCP , 0-> layer beneath the socket is network layer
+    int socketFD =  socket(AF_INET, SOCK_STREAM, 0);
+
+    char* ip = "10.40.19.245";
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    
+
+
+}
